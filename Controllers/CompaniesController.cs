1671bce3b68e1190fc69577de768e3e13ddf4ee8
Viswanathan Meenatchisundaram <Viswanathan.MS@rencata.com>
2023-01-18 09:04:18 -0800

    Controllers and views added

---
+++ Controllers/CompaniesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Placement.Portal.Skillup.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
